46d39c9f70602d5c8c8392c5e54bb13e811b0d1d
sorawalker <nikitka.grishanov@gmail.com>
2024-10-15 14:57:58 +0300

    feat (index.ts): Closure in init

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,19 @@ async function init({ token, appName }: {
     token: string,
     appName: string,
 }){
-    const app = new App(token, appName);
-    app.init()
+    let isInitialized: boolean = false;
+
+    async function initialize(token: string, appName: string) {
+        if (!isInitialized) {
+            isInitialized = true;
+            const app = new App(token, appName);
+            app.init();
+        } else {
+            console.log('Analytics have already been initialized');
+        }
+    }
+
+    return initialize(token, appName);
 }
 
 export default {
